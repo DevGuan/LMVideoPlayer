6002f40cce5c547b062278fce2c37fc18d6cabf8
lingtingSir <363905547@qq.com>
2016-03-22 13:04:04 +0800

    add videoPlayer

---
+++ LMVideoPlayer/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMVideoPlayer
+//
+//  Created by lmj  on 16/3/22.
+//  Copyright (c) 2016年 lmj . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMVideoPlayer/ViewController/MainViewController.m
@@ -0,0 +1,34 @@
+//
+//  MainViewController.m
+//  LMVideoPlayer
+//
+//  Created by lmj  on 16/3/22.
+//  Copyright (c) 2016年 lmj . All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "LMVideoPlayerOperationView.h"
+@interface MainViewController ()
+
+@property (nonatomic, strong) LMVideoPlayerOperationView *playView;
+
+
+@end
+
+@implementation MainViewController
+
+//-(BOOL)prefersStatusBarHidden{
+//    return YES;
+//}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //    [self setNeedsStatusBarAppearanceUpdate];
+    _playView = [[LMVideoPlayerOperationView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 300) videoURLString:@"http://flv2.bn.netease.com/videolib3/1603/18/Kjxcu2321/SD/Kjxcu2321-mobile.mp4"];
+    [_playView play];
+    [self.view addSubview:_playView];
+    
+}
+
+
+@end
